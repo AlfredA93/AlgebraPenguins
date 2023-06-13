57181a64fc0c00b608f3f0cabfeb93d9d4e6d8f9
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-06-13 13:02:58 +0200

    Add commented sections for script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 // Add JavaScript here
+
+// Function for Start/Stop Timer Buttons which then trigger the function to run the game.
+
+// If statement for difficulty selector
+
+// Function to create equations for each difficulty
+
+// Function to calculate the value of 'n'
+
+// Function to check the user answer
+
+// Function to check current score and add 1 if the user answer is correct
