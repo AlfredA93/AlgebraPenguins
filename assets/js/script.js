78f3bc67e415aa2ac50d088ebba5e1896881ee9f
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-06-13 13:32:54 +0200

    Add if statement for difficulty selector to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,21 @@
 // Function for Start/Stop Timer Buttons which then trigger the function to run the game.
 
 // If statement for difficulty selector
+if (difficulty === "easy") {
+    let num1 = Math.ceil(Math.random() * 5);
+    easyEquation(num1);
+} else if (difficulty === "normal") {
+    let num1 = Math.ceil(Math.random() * 10);
+    let num2 = Math.ceil(Math.random() * 10);
+    normalEquation(num1, num2);
+} else if (difficulty === "hard") {
+    let num1 = Math.ceil(Math.random() * 20);
+    let num2 = Math.ceil(Math.random()*4);
+    hardEquation(num1, num2);
+} else {
+    alert(`Please select difficulty.`);
+    throw `Please select difficulty. Aborting.`;
+}
 
 // Function to create equations for each difficulty
 
